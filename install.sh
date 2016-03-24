e1ce51380059a05c7ef494e1e2ed77a0f72a51fe
Alisam Technology <AlisamTechnology@users.noreply.github.com>
2016-03-24 22:30:46 +0100

    Install Script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+echo "[]====================================================================[]"
+echo "[]                     ALISAM  TECHNOLOGY                             []"
+echo "[]                  Atscan Tool Install Script                        []"
+echo "[]====================================================================[]"
+echo "[!] Install.sh will install atscan tool in the system!![y/n]" ; 
+read baba
+
+if [ $baba == "y" ] ; 
+then
+    echo " "
+else
+    exit
+fi
+
+if [ -d "/usr/share/doc/ATSCAN" ] ; 
+then
+echo "[!] A directory ATSCAN was found! Do you want to replace it?[y/n]" ; 
+read mama
+
+if [ $mama == "y" ] ; 
+then
+  rm -R "/usr/share/doc/ATSCAN/"
+else
+  exit
+fi
+fi
+
+git clone https://github.com/AlisamTechnology/ATSCAN.git /usr/share/doc/ATSCAN/
+echo '#!/bin/bash
+perl /usr/share/doc/ATSCAN/atscan.pl "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9"' > atscan
+chmod +x atscan
+sudo cp atscan /usr/bin/
+echo "[]====================================================================[]"
+echo "[]     All is done!! You can execute Atscan by typing atscan !        []" 
+echo "[]====================================================================[]"
+rm atscan

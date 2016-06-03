cf24dbb81da3662da0b0e58cc67e75d3aa736868
Alisam Technology <AlisamTechnology@users.noreply.github.com>
2016-06-03 04:24:38 +0200

    Now install everywhere

--- install.sh
+++ install.sh
@@ -1,46 +1,49 @@
 #!/bin/bash
-echo "[ ]====================================================================[ ]"
-echo "[ ]                     ALISAM  TECHNOLOGY                             [ ]"
-echo "[ ]                  Atscan Tool Install Script                        [ ]"
-echo "[ ]====================================================================[ ]"
+echo "[ ]====================================================================[ ]";
+echo "[ ]                     ALISAM  TECHNOLOGY                             [ ]";
+echo "[ ]                  Atscan Tool Install Script                        [ ]";
+echo "[ ]====================================================================[ ]";
+echo "";
 echo "[!] Install.sh will install atscan tool in the system [Y/n]" ; 
 read baba
-
 if [ $baba == "y" ] ; 
 then
-    echo " "
+echo "[!] Where Do you want to install the tool? [Ex:/usr/share/doc]:";
+read refdir
+echo "[ ] Checking directories..."
+if [ -d "$refdir/ATSCAN" ] ;
+then
+echo "[!] A directory ATSCAN was found! Do you want to replace it? [Y/n]:" ; 
+read mama
+if [ $mama == "y" ] ; 
+then
+ rm -R "$refdir/ATSCAN"
+
+ echo "[!] Installing ...";
+ echo "";
+ git clone https://github.com/AlisamTechnology/ATSCAN.git $refdir/ATSCAN;
+ echo "#!/bin/bash 
+ perl $refdir/ATSCAN/atscan.pl" '${1+"$@"}' > atscan;
+ chmod +x atscan;
+ sudo cp atscan /usr/bin/;
+ rm atscan;
+
 else
     exit
 fi
+fi
+fi
 
-echo "[ ] Checking directories..."
-
-if [ -d "/usr/share/doc/ATSCAN" ] ; 
+if [ -d "$refdir/ATSCAN" ] ;
 then
-echo "[!] A directory ATSCAN was found! Do you want to replace it? [Y/n]" ; 
-read mama
-
-if [ $mama == "y" ] ; 
-then
-  rm -R "/usr/share/doc/ATSCAN/"
+echo "";
+echo "Tool istalled with success!";
+echo "";
+  echo "[ ]====================================================================[ ]";
+  echo "[ ]     All is done!! You can execute Atscan by typing atscan !        [ ]"; 
+  echo "[ ]====================================================================[ ]";
+  echo "";
 else
+  echo "[!] Installation faid!! ";
   exit
-fi
-fi
-echo "[!] Installing ..."
-git clone https://github.com/AlisamTechnology/ATSCAN.git /usr/share/doc/ATSCAN/
-echo '#!/bin/bash
-perl /usr/share/doc/ATSCAN/atscan.pl ${1+"$@"}' > atscan
-chmod +x atscan
-sudo cp atscan /usr/bin/
-rm atscan
-
-if [ -d "/usr/share/doc/ATSCAN" ] ; 
-then
-  echo "[ ]====================================================================[ ]"
-  echo "[ ]     All is done!! You can execute Atscan by typing atscan !        [ ]" 
-  echo "[ ]====================================================================[ ]"
-else
-  echo "[!] Install faid!! "
-exit
 fi

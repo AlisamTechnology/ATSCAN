e2389c71426c618ab5779319499387e8272c5dd7
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-06-28 01:10:17 +0200

    Update atscan.pl

--- atscan.pl
+++ atscan.pl
@@ -1188,9 +1188,10 @@ sub checkVersion {
   my $html = $response->content;
   if ($response->is_success) { 
     unlink $Bin."/atscan.pl";        
-	open (FILE, '>>', "$Bin/atscan.pl");
+	open (FILE, '>', "$Bin/atscan.pl");
     print FILE $response->content;
     close (FILE);
+    system $^O eq 'MSWin32' ? 'cls' : 'clear';  
     system("chmod +x $Bin/atscan.pl | perl $Bin/atscan.pl || atscan");
     print $c[3]."[!] $DT[7]\n";
   }

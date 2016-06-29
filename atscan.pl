418216a31521c6afd95b4fdf6f7b7962b7dc4d5d
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-06-29 23:00:22 +0200

    Update atscan.pl

--- atscan.pl
+++ atscan.pl
@@ -1194,7 +1194,7 @@ sub checkVersion {
     close (FI);    
     use File::Compare;      
     if (compare($script_bac, $script) == 0) {
-      print mtak(); ltak(); $c[4]."  $DT[6]\n"; }
+      mtak(); ltak(); print $c[4]."  $DT[6]\n"; }
     else{
 	  open (FILE, '>', $script);
       print FILE $response->content;

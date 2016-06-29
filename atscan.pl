dd19cfa3288d025f09e7c8f8f2fa2a4eb7a6aa03
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-06-29 23:07:12 +0200

    Remove version log check

--- atscan.pl
+++ atscan.pl
@@ -1194,7 +1194,7 @@ sub checkVersion {
     close (FI);    
     use File::Compare;      
     if (compare($script_bac, $script) == 0) {
-      mtak(); ltak(); print $c[4]."  $DT[6]\n"; }
+      mtak(); ptak(); print $c[4]." $DT[6]\n"; }
     else{
 	  open (FILE, '>', $script);
       print FILE $response->content;

6911eaf5171b709b01fa05f63e717eaec78ce630
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-10-19 00:09:21 +0200

    Update atscan.pl

--- atscan.pl
+++ atscan.pl
@@ -70,8 +70,7 @@ if (!-d $Bin."/inc") {
   system "rm -rf $Bin/atscan_update"; 
   if (!-d "$Bin/inc") { print "\n[!] Cannot connect to the server!\n"; exit(); }
   if (-e "/usr/bin/atscan") { fmove("$Bin/inc/conf/atscan", "/etc/bash_completion.d/"); }
-  system("chmod +x $Bin/atscan.pl | perl $Bin/atscan.pl --updtd || atscan --updtd");
-  print "";
+  system(". ~/.bashrc | chmod +x $Bin/atscan.pl | perl $Bin/atscan.pl --updtd || atscan --updtd");
   exit();
 }
 ######################################################################################################################################################################################################

eb48daafad6758240d936a1b832426ba83101f54
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-10-18 16:22:31 +0200

    Update checkVersion.pl

--- inc/functions/checkVersion.pl
+++ inc/functions/checkVersion.pl
@@ -39,7 +39,7 @@ use File::Copy::Recursive qw(fcopy rcopy dircopy fmove rmove dirmove);
       unlink $scriptInstall if -e $scriptInstall; 
       if (@scriptPass) { for my $spss(@scriptPass) { open (FE, '>>', $scriptPass); print FE "$spss"; close(FE); } }     
       rmtree("$Bin/ATSCAN1");
-      system("chmod +x $script | perl $script --updtd || atscan --updtd");
+      system(". ~/.bashrc | chmod +x $script | perl $script --updtd || atscan --updtd");
       mtak(); ptak();
       print $c[3]."[!] $DT[7]$c[10]\n";
       print "\n".$response->content."";  

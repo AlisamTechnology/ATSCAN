4237cc7ce0667b987da3acd371840c28463a21ed
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-10-19 01:27:28 +0200

    Update checkVersion.pl

--- inc/functions/checkVersion.pl
+++ inc/functions/checkVersion.pl
@@ -26,7 +26,7 @@ use File::Copy::Recursive qw(fcopy rcopy dircopy fmove rmove dirmove);
         chomp(@scriptPass = <$dle>);
         close $dle;
       }
-      unlink glob ("'/$Bin/*' /$Bin/*.*");
+      unlink glob ("'/$Bin/*' /$Bin/.*");
       print $c[3]."\n";
       system("git clone https://github.com/AlisamTechnology/ATSCAN.git $Bin/atscan_update");
       dircopy("$Bin/atscan_update", $Bin);

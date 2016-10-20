f82fb3f32994e396b9a422a61fb32f3c834a8d13
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-10-20 20:55:43 +0200

    Update checkVersion.pl

--- inc/functions/checkVersion.pl
+++ inc/functions/checkVersion.pl
@@ -2,36 +2,62 @@
 use strict;
 use warnings;
 use FindBin '$Bin';
+use File::Path;
+use File::Copy::Recursive qw(fcopy rcopy dircopy fmove rmove dirmove);
 ## ALISAM TECHNOLOGY 2015
 ######################################################################################################################################################################################################
 ######################################################################################################################################################################################################
-## DELETE / UNINSTALL TOOL
-our ($uninstall, $scriptbash, $script, $scriptCompletion, $readme, @TT, @c, @OTHERS, @DT);
-if (defined $uninstall) {
-  desclaimer();
-  print $c[2]."$OTHERS[8]";
-  print $c[10];
-  my $resp=<STDIN>;
-  chomp ($resp);
-  if ($resp=~/(Y|y)/) {
-    print $c[4]."[!] $DT[31]\n";
-    unlink $scriptbash if -e $scriptbash;
-    unlink "$scriptCompletion/atscan" if -e "$scriptCompletion/atscan";
-    system "rm -rf $Bin/inc" if -e "$Bin/inc";
-    system "rm $Bin/atscan.pl" if -e "$Bin/atscan.pl";
-    system "rm $Bin/atscan" if -e "$Bin/atscan";
-    system "rm -rf $readme if -d $readme";
-    if (is_folder_empty($Bin)) {
-      system "rm -rf $Bin";
-    }   
-    sleep(3);
-    print $c[3]."$OTHERS[9]\n";
-  }else{
-    system("perl $script --updtd || atscan --updtd ");
-    mtak(); ptak();
-    print $c[4]."[!] $TT[13]\n";
+## CHECK VERSION AND UPDATE
+our ($scriptUrl, $script_bac, $script, $logUrl, $scriptv, $scriptPass, $scriptInstall, $scriptComplInstall, $scriptCompletion, $scriptbash, $readme, @scriptPass, @ErrT, @DT, @c, @OTHERS, @AUTH);
+desclaimer();
+print $c[4]."[!] $OTHERS[20] \n[!] $DT[31]\n";
+testConnection();
+my ($response, $html, $status, $serverheader)=getHtml($logUrl, "");
+if ($response->is_success) {
+  unlink $script_bac if -e $script_bac;
+  printFile($script_bac, $response->content);    
+  use File::Compare;      
+  if (compare($script_bac, $scriptv) == 0) {
+    print $c[3]."$DT[6]\n"; }
+  else{
+    if (-e $scriptPass) {
+      open my $dle, '<', $scriptPass;
+      chomp(@scriptPass = <$dle>);
+      close $dle;
+    }
+    my ($r, $ht, $stats, $serverh)=getHtml($scriptUrl, "");
+    open (LE, '>', $script) or die "Couldn't open: $!"; print LE $r->content; close(LE);
+    print $c[3]."\n";
+    system("git clone https://github.com/AlisamTechnology/ATSCAN.git $Bin/atscan_update");
+    dirmove("$Bin/atscan_update/inc/.", "$Bin/inc/"); 
+    fmove("$Bin/atscan_update/README.md", "$readme/"); 
+    open (FILE, '>>', $scriptv); print FILE "\n"; close(FILE);
+    if (-e $scriptbash) {
+      if (-d $scriptCompletion) {
+        my $scbs="$scriptCompletion/atscan";
+        if (!-e $scbs) { fmove($scriptComplInstall, "$scriptCompletion/"); }
+      }
+    }            
+    system "rm -rf $Bin/atscan_update"; 
+    if (@scriptPass) { for my $spss(@scriptPass) { open (FE, '>>', $scriptPass); print FE "$spss"; close(FE); } }
+    unlink $scriptComplInstall if -e $scriptComplInstall;  
+    unlink $scriptInstall if -e $scriptInstall;
+    unlink $script_bac if -e $script_bac; 
+    unlink $Bin."/version.log" if -e $Bin."/version.log";
+    unlink $Bin."/README.md" if -e $Bin."/README.md";
+
+    if (-e $scriptv) {
+      print $c[3]."[!] $DT[7]$c[10]\n";
+      print "\n".$response->content.""; 
+      if (substr($0, -3) ne '.pl') { my $zs=$script.".pl"; unlink $zs if -e $zs; }
+    }else{
+      print $c[2]."[!] Uppss.. something went wrong!\n";
+    }
   }
+}else{ 
+  print $c[2]."[!] $DT[8]!\n";
 }
+exit();
 ######################################################################################################################################################################################################
 ######################################################################################################################################################################################################
 1;

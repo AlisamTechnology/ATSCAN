52b1260301c1af07782da5469bc78b73fd345234
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-10-21 00:48:56 +0200

    Update uninstall.pl

--- inc/functions/uninstall.pl
+++ inc/functions/uninstall.pl
@@ -15,15 +15,18 @@ if (defined $uninstall) {
   chomp ($resp);
   if ($resp=~/(Y|y)/) {
     print $c[4]."[!] $DT[31]\n";
+    print $c[10]."[!] Removing ...\n";
+ 
     unlink $scriptbash if -e $scriptbash;
     unlink "$scriptCompletion/atscan" if -e "$scriptCompletion/atscan";
     system "rm -rf $Bin/inc" if -e "$Bin/inc";
     system "rm $Bin/atscan.pl" if -e "$Bin/atscan.pl";
     system "rm $Bin/atscan" if -e "$Bin/atscan";
     system "rm -rf $readme if -d $readme";
+    
     if (is_folder_empty($Bin)) {
       system "rm -rf $Bin";
-    }else{ print "$Bin still have files\n";}
+    }
     sleep(3);
     print $c[3]."$OTHERS[9]\n";
   }else{

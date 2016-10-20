74cd25de227636cff1d0c084002ce5e1979aa04f
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-10-20 17:22:26 +0200

    Update checkVersion.pl

--- inc/functions/checkVersion.pl
+++ inc/functions/checkVersion.pl
@@ -8,7 +8,7 @@ use File::Copy::Recursive qw(fcopy rcopy dircopy fmove rmove dirmove);
 ######################################################################################################################################################################################################
 ######################################################################################################################################################################################################
 ## CHECK VERSION AND UPDATE
-our ($scriptUrl, $script_bac, $script, $logUrl, $scriptv, $scriptPass, $scriptInstall, $scriptComplInstall, $scriptCompletion, $scriptbash, @scriptPass, @ErrT, @DT, @c, @OTHERS);
+our ($scriptUrl, $script_bac, $script, $logUrl, $scriptv, $scriptPass, $scriptInstall, $scriptComplInstall, $scriptCompletion, $scriptbash, @scriptPass, @ErrT, @DT, @c, @OTHERS, @AUTH);
 desclaimer();
 print $c[4]."[!] $OTHERS[20] \n[!] $DT[31]\n";
 testConnection();
@@ -29,7 +29,7 @@ if ($response->is_success) {
     open (LE, '>', $script) or die "Couldn't open: $!"; print LE $r->content; close(LE);
     print $c[3]."\n";
     system("git clone https://github.com/AlisamTechnology/ATSCAN.git $Bin/atscan_update");
-    dircopy("$Bin/atscan_update/inc/", "$Bin/inc/");      
+    dircopy("$Bin/atscan_update/inc", $Bin);      
     open (FILE, '>>', $scriptv); print FILE "\n"; close(FILE);
     if (-e $scriptbash) {
       if (-d $scriptCompletion) {
@@ -41,11 +41,27 @@ if ($response->is_success) {
     if (@scriptPass) { for my $spss(@scriptPass) { open (FE, '>>', $scriptPass); print FE "$spss"; close(FE); } }
     unlink $scriptComplInstall if -e $scriptComplInstall;  
     unlink $scriptInstall if -e $scriptInstall;
+
+    if (-e "$Bin/inc") {
+      print $c[3]."[!] $DT[7]$c[10]\n";
+      print "\n".$response->content.""; 
+    }else{ print $c[2]."[!] $DT[8]!\n"; }
+    
     if (substr($0, -3) ne '.pl') { my $zs=$script.".pl"; unlink $zs if -e $zs; }
+
+    use Time::HiRes qw(usleep);
+    local $| = 1;
+    my @nums = -5 .. -1;
+    print $c[3]."[!] $AUTH[6] Tool will restart in ";
+    foreach my $c (@nums) {
+      print "$c";
+      usleep(1000000);
+      print ("\b" x length($c));
+    }
+    print "\n";
     system(". ~/.bashrc | chmod +x $script | perl $script --updtd || atscan --updtd");
     mtak(); ptak();
     print $c[3]."[!] $DT[7]$c[10]\n";
-    print "\n".$response->content.""; 
   }
   unlink $script_bac if -e $script_bac; 
   unlink $Bin."/version.log" if -e $Bin."/version.log";

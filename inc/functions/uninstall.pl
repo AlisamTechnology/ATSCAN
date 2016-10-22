14646dd60a7b855c1146be8cf13506b2cb70481f
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-10-23 00:42:16 +0200

    Update uninstall.pl

--- inc/functions/uninstall.pl
+++ inc/functions/uninstall.pl
@@ -6,7 +6,7 @@ use FindBin '$Bin';
 ######################################################################################################################################################################################################
 ######################################################################################################################################################################################################
 ## DELETE / UNINSTALL TOOL
-our ($uninstall, $scriptbash, $script, $scriptCompletion, $readme, @TT, @c, @OTHERS, @DT);
+our ($uninstall, $scriptbash, $script, $scriptCompletion, $readme, @TT, @c, @OTHERS, @DT, @ZT);
 if (defined $uninstall) {
   print $c[2]."$OTHERS[8]";
   print $c[10];
@@ -15,16 +15,16 @@ if (defined $uninstall) {
   if ($resp=~/(Y|y)/) {
     my @uninstall=($scriptbash, "$scriptCompletion/atscan", $script);
     for my $mm(@uninstall) {
-      print $c[10]."[!] Removing $mm... ";
+      print $c[10]."[!] $ZT[8] $mm... ";
       unlink $mm if -e $mm;
       if (!-e $mm) { cc(); }else{ bb(); }
     }
     
-    print $c[10]."[!] Removing $Bin/inc... ";
+    print $c[10]."[!] $ZT[8] $Bin/inc... ";
     system "rm -rf $Bin/inc" if -e "$Bin/inc";
     if (!-d "$Bin/inc") { cc(); }else{ bb(); }
     
-    print $c[10]."[!] Removing $readme... ";
+    print $c[10]."[!] $ZT[8] $readme... ";
     system "rm -rf $readme if -d $readme";
     if (!-d $readme) { cc(); }else{ bb(); }
     

be0774f4a740e9cad7ca4857d08a7e28c93b21ac
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-10-24 06:17:41 +0200

    Update upad.pl

--- inc/conf/upad.pl
+++ inc/conf/upad.pl
@@ -9,25 +9,33 @@ use File::Path;
 our ($uplog, $script_bac, $fulldate, @c);
 my (@date1, @date2);
 
+## date of last update
 open (UPLOG, $uplog);
 while (my $lg=<UPLOG>) {
   chomp $lg;
-  @date1=split(":", $lg);   
+  @date1=split(":", $lg);
 }
-@date2=split(":", $fulldate);    
 
+## day number + current day
+@date2=split(":", $fulldate); ## days number + current day 
+
+## if date of last update is great than 1 year
 if ($date1[1]<$date2[1]) {
   updateMessage();
 }else{
+  ## check i day number is great than 10 days : check updates frequency
   if (($date1[0]+10)<$date2[0]) {
     updateMessage();
   }
 }
 
+## print update message
 sub updateMessage  {
-  my ($same, $response)=compareme();
-  if (!$same) {
-    print $c[4]."  [!] A new update is aviable! To update: --update\n";
+  my ($same, $rp)=compareme();
+  if ($rp->is_success) {  
+    if ($same==1) { print $c[4]."  [!] A new update is aviable! To update: --update\n"; }
+  }else{
+    print "  "; dd();
   }
 }      
 

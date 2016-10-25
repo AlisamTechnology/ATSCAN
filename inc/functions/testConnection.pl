9b267fbadc25bd1eebada42c4936e2b3f8a28553
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2016-10-25 08:51:54 +0200

    Update testConnection.pl

--- inc/functions/testConnection.pl
+++ inc/functions/testConnection.pl
@@ -11,6 +11,7 @@ sub testConnection {
   if (defined $proxy || defined $prandom) { print $c[4]."[!] $ErrT[20] [$psx].. "; UA(); }
   my ($response, $html, $status, $serverheader)=getHtml($ipUrl, "");
   if (!$response->is_success) {
+    if (defined $proxy || defined $prandom) { print "\n"; }  
     print $c[2]."[!] $DT[11]\n[!] $DT[10]\n".$c[4]."[!] $ErrT[23]\n"; logoff();
   }else{
     if (defined $proxy || defined $prandom) { print $c[3]."[!] OK!\n"; ltak(); sleep(1); }    

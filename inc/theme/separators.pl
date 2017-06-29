3e6e034d536bb8b317d567a19eccbb4538cd58e7
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2017-06-30 00:18:09 +0200

    Update separators.pl

--- inc/theme/separators.pl
+++ inc/theme/separators.pl
@@ -6,13 +6,13 @@ use FindBin '$Bin';
 
 ## SEPARATIONS
 our (@c);
-sub mtak { my $sn1="_" x 82; print $c[0]."$sn1\n"; }
-sub dpoints { my $sn2=":" x 78; print $c[0]."    $sn2\n"; }
-sub ptak { my $sn3="-" x 82; print $c[1]."$sn3\n"; }
-sub points  { my $sn4="=" x 78; print $c[0]."    $sn4\n"; }
-sub ltak { my $sn5="-" x 82; print $c[1]."$sn5\n"; }
-sub stak { my $sn6="-" x 78; print $c[1]."    $sn6\n"; }
-sub stakScan { my $ee=":" x 70; print $c[0]."$ee\n"; }
+sub mtak { my $sn1="_" x 80; print $c[0]."$sn1\n"; }
+sub dpoints { my $sn2=":" x 76; print $c[0]."    $sn2\n"; }
+sub ptak { my $sn3="-" x 80; print $c[1]."$sn3\n"; }
+sub points  { my $sn4="=" x 76; print $c[0]."    $sn4\n"; }
+sub ltak { my $sn5="-" x 80; print $c[1]."$sn5\n"; }
+sub stak { my $sn6="-" x 76; print $c[1]."    $sn6\n"; }
+sub stakScan { my $ee=":" x 68; print $c[0]."$ee\n"; }
 our $sp=" " x 11;
 
 1;

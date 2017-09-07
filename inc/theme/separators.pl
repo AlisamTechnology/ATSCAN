e6a06b0b506e22b68956f29c4a8bf12ec6d7c3f2
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2017-09-07 15:11:12 +0200

    Add files via upload

--- inc/theme/separators.pl
+++ inc/theme/separators.pl
@@ -7,7 +7,7 @@ use FindBin '$Bin';
 ## SEPARATIONS
 our (@c);
 sub mtak { my $sn1="_" x 80; print $c[1]."$sn1\n"; }
-sub dpoints { my $sn2=":" x 76; print $c[1]."    $sn2\n"; }
+sub dpoints { my $sn2=":" x 66; print $c[1]."    "; timer(); print "$sn2\n"; }
 sub ptak { my $sn3="-" x 80; print $c[1]."$sn3\n"; }
 sub points  { my $sn4="=" x 76; print $c[1]."    $sn4\n"; }
 sub ltak { my $sn5="-" x 80; print $c[1]."$sn5\n"; }

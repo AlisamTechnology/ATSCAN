686d4ce9d69b3c26c7b9200ae731509b771504c5
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2017-10-01 17:56:34 +0200

    Add files via upload

--- inc/header.pl
+++ inc/header.pl
@@ -9,7 +9,7 @@ our (@OTHERS, @c, @TT);
 ## ADVISE
 sub advise {
   if (-e "/usr/bin/atscan") {
-    print $c[5]."[!] $OTHERS[5]: $TT[7]\n";
+    print $c[11]."[!] $OTHERS[5]:$c[5] atscan$c[11] [$c[5]options$c[11]] or $c[5]atscan$c[11] [$c[5]--help$c[11]|$c[5]-h$c[11]|$c[5]-?$c[11]] or $c[5]atscan$c[11] [$c[5]--interactive$c[11]]\n";
   }else{
     print $c[5]."[!] $OTHERS[5]: perl ./",basename($0)," $TT[8] ./",basename($0)," $TT[9]\n";
   }

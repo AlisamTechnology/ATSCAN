0380d0fd44f8b6c23b60a296972136e5580cba32
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2018-06-21 14:48:08 +0200

    Add files via upload

--- inc/functions/checkVersion.pl
+++ inc/functions/checkVersion.pl
@@ -9,6 +9,8 @@ use File::Path;
 our ($scriptUrl, $script_bac, $script, $logUrl, $scriptv, $userSetting, $scriptInstall, $scriptComplInstall, $scriptCompletion, $scriptbash, $readme, $uplog, $fulldate, $interactive,
      @userSetting, @ErrT, @DT, @c, @OTHERS, @AUTH, @ZT);
 
+my $er="[!] Error! Cannot";
+#############################################
 print $c[4]."[i] $OTHERS[20]\n";
 my ($same, $response)=compareme();
 if ($same eq "yes") {   
@@ -16,122 +18,136 @@ if ($same eq "yes") {
 }else{
   print $c[3]."[i] An update is aviable!\n";
   if (-e $userSetting) {
-    print $c[10]."[i] $ZT[0] ";
+    print $c[10]."[i] $ZT[0]...\n";
     open my $dle, '<', $userSetting or nochmod($userSetting, "");
     chomp(@userSetting = <$dle>);
     close $dle;
-    cc();
   }
     
   my ($r, $ht, $stats, $serverh)=getHtml($scriptUrl, "");
   if (!$r->is_success) { print "\n"; dd(); exit(); }
-    
-  print $c[10]."[i] $ZT[1] ";
+   
+  #############################################  
+  print $c[10]."[i] $ZT[1]...\n";
   open (LE, '>', $script) or nochmod($script, "exit");
   print LE $r->decoded_content; close(LE);
-  cc();
-    
+  
+  #############################################
   print $c[10];
   system("git clone https://github.com/AlisamTechnology/ATSCAN.git $Bin/atscan_update");
   if (!-d "$Bin/atscan_update") { print "\n"; dd(); exit(); }
-  print $c[10]."[i] $ZT[2] ";
+  print $c[10]."[i] $ZT[2]..\n";
   system "sudo cp -r $Bin/atscan_update/inc $Bin";
   if (!-d "$Bin/inc") { bb(); exit(); }
-  else{ cc(); }
+  sleep(1);
   
+  #############################################
   if (-d "/usr/share/doc/atscan") {
     my @f=("README.md", "License.txt");
+    print $c[10]."[i] $ZT[5] License files...\n";
     for my $f(@f) {
-      print $c[10]."[i] $ZT[5] $f to $readme/... ";
       system "sudo cp -r $Bin/atscan_update/$f /usr/share/doc/atscan/";
-      if (!-e "/usr/share/doc/atscan/$f") { bb(); }
-      else{ cc(); }
+      if (!-e "/usr/share/doc/atscan/$f") {
+        print $c[3]."$er write to /usr/share/doc/atscan/$f!\n";
+      }
     }
   }
-  print $c[10]."[i] $ZT[4] ";
+  sleep(1);
+  
+  ##############################################
+  print $c[10]."[i] $ZT[4]...\n";
   open (FILE, '>>', $scriptv) or nochmod($scriptv, "");
   print FILE "\n"; close(FILE);
-  cc();  
-    
+  sleep(1);
+  
+  ################################################  
   if (-e $scriptbash) {
     if (-d $scriptCompletion) {
       my $scbs="$scriptCompletion/atscan";        
       unlink $scbs if -e $scbs;
-      print $c[10]."[i] $ZT[5] $scriptComplInstall to $scriptCompletion/... ";
+      print $c[10]."[i] $ZT[5] bash Completion... \n";
       system "sudo cp $scriptComplInstall $scriptCompletion/";
-      if (!-e "$scriptCompletion/atscan") { bb(); }
-      else{ cc(); }
+      if (!-e "$scriptCompletion/atscan") {
+        print $c[3]."$er write to $scriptCompletion!\n";
+      }
+      sleep(1);
+
+      #############################################
       print $c[10]."[i] Creating desktop entries....\n";
-        
       if (-d "/usr/share/icons/hicolor/16x16/apps/") {
 	    system "sudo cp $Bin/atscan_update/inc/conf/desktop/16x16/atscan-menu.png /usr/share/icons/hicolor/16x16/apps/";
-	    if (!-e "/usr/share/icons/hicolor/16x16/apps/atscan-menu.png") { print $c[3]."[!] Error cannot whrite in /usr/share/icons/hicolor/16x16/apps/!\n"; }
+	    if (!-e "/usr/share/icons/hicolor/16x16/apps/atscan-menu.png") { print $c[3]."$er write in /usr/share/icons/hicolor/16x16/apps/!\n"; }
 	
 	    system "sudo cp $Bin/atscan_update/inc/conf/desktop/22x22/atscan-menu.png /usr/share/icons/hicolor/22x22/apps/";
-	    if (!-e "/usr/share/icons/hicolor/22x22/apps/atscan-menu.png") { print $c[3]."[!] Error cannot whrite in /usr/share/icons/hicolor/22x22/apps/!\n"; }
+	    if (!-e "/usr/share/icons/hicolor/22x22/apps/atscan-menu.png") { print $c[3]."$er write in /usr/share/icons/hicolor/22x22/apps/!\n"; }
 	
 	    system "sudo cp $Bin/atscan_update/inc/conf/desktop/24x24/atscan-menu.png /usr/share/icons/hicolor/24x24/apps/";
-	    if (!-e "/usr/share/icons/hicolor/24x24/apps/atscan-menu.png") { print $c[3]."[!] Error cannot whrite in /usr/share/icons/hicolor/24x24/apps/!\n"; }
+	    if (!-e "/usr/share/icons/hicolor/24x24/apps/atscan-menu.png") { print $c[3]."$er write in /usr/share/icons/hicolor/24x24/apps/!\n"; }
 	
 	    system "sudo cp $Bin/atscan_update/inc/conf/desktop/48x48/atscan-menu.png /usr/share/icons/hicolor/48x48/apps/";
-	    if (!-e "/usr/share/icons/hicolor/48x48/apps/atscan-menu.png") { print $c[3]."[!] Error cannot whrite in /usr/share/icons/hicolor/48x48/apps/!\n"; }
+	    if (!-e "/usr/share/icons/hicolor/48x48/apps/atscan-menu.png") { print $c[3]."$er write in /usr/share/icons/hicolor/48x48/apps/!\n"; }
 	
 	    system "sudo cp $Bin/atscan_update/inc/conf/desktop/256x256/atscan-menu.png /usr/share/icons/hicolor/256x256/apps/";
-	    if (!-e "/usr/share/icons/hicolor/256x256/apps/atscan-menu.png") { print $c[3]."[!] Error cannot whrite in /usr/share/icons/hicolor/256x256/apps/!\n"; }
+	    if (!-e "/usr/share/icons/hicolor/256x256/apps/atscan-menu.png") { print $c[3]."$er write in /usr/share/icons/hicolor/256x256/apps/!\n"; }
      
         my @deskFiles=("/usr/share/icons/hicolor/16x16/apps/atscan-menu.png", "/usr/share/icons/hicolor/22x22/apps/atscan-menu.png", "/usr/share/icons/hicolor/24x24/apps/atscan-menu.png",
                        "/usr/share/icons/hicolor/48x48/apps/atscan-menu.png", "/usr/share/icons/hicolor/256x256/apps/atscan-menu.png");
-        for my $deskFile(@deskFiles) {
-          print $c[10]."[i] $ZT[5] $deskFile ... ";
-          if (!-e "$deskFile") { print $c[3]."[!] Error cannot whrite to $deskFile!\n"; }
-          else{ cc();
-          }
-        }
       }
     }
   }
+  sleep(1);
   
-  print $c[10]."[i] $ZT[9] ";
+  ##############################################
+  print $c[10]."[i] $ZT[9]...\n";
   open (MN, '>', $uplog) or nochmod($uplog, "");
   print MN "$fulldate"; close(MN);
   sleep(1);
-  cc();
   
+  ##############################################
   unlink $userSetting if -e $userSetting;   
   if (@userSetting) {
-    print $c[10]."[i] $ZT[6] ";
+    print $c[10]."[i] $ZT[6]... \n";
     open (FE, '>>', $userSetting) or nochmod($userSetting, "");
     for my $spss(@userSetting) {
       print FE "$spss\n";
     }
     close(FE);
-    cc();
   }  
-  
-  print $c[10]."[i] Removing Desktop entries ";
+  sleep(1);
+
+  ###############################################
+  print $c[10]."[i] Removing Desktop entries...\n";
   system "rm -rf $Bin/inc/conf/desktop";
-  if (!-d "$Bin/inc/conf/desktop") { cc(); }
-  else{ bb(); }
+  if (-d "$Bin/inc/conf/desktop") {
+    print $c[3]."$er remove $Bin/inc/conf/desktop!\n";
+  }
+  sleep(1);
 
-  print $c[10]."[i] $ZT[7] ";
+  ###############################################
+  print $c[10]."[i] $ZT[7].. \n";
   system "rm -rf $Bin/atscan_update";
-  if (!-d "$Bin/atscan_update") { cc(); }
-  else{ bb(); }
-
+  if (-d "$Bin/atscan_update") {
+    print $c[3]."$er remove $Bin/atscan_update!\n";
+  }
+  sleep(1);
+  
+  ###############################################
   my @unlinks=($scriptComplInstall, $scriptInstall, $Bin."/README.md", $Bin."/License.txt", $script_bac);
   for my $unlink(@unlinks) {
-    print $c[10]."[i] $ZT[8] $unlink... ";
     unlink $unlink if -e $unlink;
-    if (!-e $unlink) { cc(); }      
+    if (-e $unlink) {
+      print $c[3]."$er remove $unlink!\n";
+    }
   }
-  
   sleep(1);
-  print $c[3]."[i] $DT[7]\n";
+  
+  #################################################
   print "$c[10]\n".$response->decoded_content.""; 
   if (substr($0, -3) ne '.pl') {
     my $zs=$script.".pl";
     unlink $zs if -e $zs or nochmod($zs, "");
   }
+  print $c[3]."[i] $DT[7]...\n";
 }
 
 1;

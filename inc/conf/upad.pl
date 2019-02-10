aa2884a6591d8326bc1d57f08e625b42fc841d92
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2019-02-10 22:32:38 +0100

    Update upad.pl

--- inc/conf/upad.pl
+++ inc/conf/upad.pl
@@ -29,7 +29,7 @@ my $lastupdate=get_lastUpdate();
 if ($dateupdate) {
   if (($dateupdate + $lastupdate) < $fulldate) { updateMessage(); }
 }else{
-  if (($lastupdate + 10) < $fulldate) { updateMessage(); }
+  if ($lastupdate ne $fulldate) { updateMessage(); }
 }
 
 1;

b6c228842071fdd6155f17b272b4ba5b55ba2f9c
Ali MEHDIOUI <AlisamTechnology@users.noreply.github.com>
2019-02-27 15:46:21 +0100

    Update configure.pl

--- inc/conf/configure.pl
+++ inc/conf/configure.pl
@@ -5,6 +5,16 @@ use FindBin '$Bin';
 use POSIX qw(strftime);
 ## Copy@right Alisam Technology see License.txt
 
+## DELETED TOOTL FILES
+my @deteted_files=(
+                   $Bin."/inc/theme/separators.pl",
+                   $Bin."/inc/theme/colors.pl",
+                   $Bin."/inc/index.pl",
+                   $Bin."/inc/functions/ArgsList.pl"
+				   );
+
+for (@deteted_files) { unlink $_ if -e $_; }
+
 ## CLEAR
 if ($^O!~/Win/) { printf "\033c"; }else{ system("cls"); }
 
